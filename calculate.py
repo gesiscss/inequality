7045076ad096346a02516c37cacef593121decc8
Kandy <kandy@spko-css-app02.gesis.org>
2017-11-17 17:59:20 +0100

    grouping, gini calculation correction, cohort participation plot and regression analysis done

--- calculate.py
+++ calculate.py
@@ -26,30 +26,42 @@ def gini(array):
 
 
 def groupDataAndCalculateCumulativeValues(data, group_year, criterion):
-    GROUP_YEAR = group_year
-
-    if GROUP_YEAR > 1:
-        MIN_YEAR = data['year'].min() 
-        MAX_YEAR = data['year'].max() + 2*GROUP_YEAR
-        yearGroups = range(MIN_YEAR, MAX_YEAR, GROUP_YEAR)
+    # data - the dataframe containing author publications or citations data
+    # group_year - how many years should be clubbed to form
+    # criterion - 'num_pub' (or) 'num_cit'
+    
+    # Group years and associative data and calculates the cumulative value
+    if group_year > 1:
+        min_year = data['year'].min() 
+        max_year = data['year'].max() + 2*group_year
+        yearGroups = range(min_year, max_year, group_year)
+        #bins - contains the year groups and labels contains all the group except the last one
         data['year'] = pd.cut(data['year'], bins=yearGroups, labels=yearGroups[:-1],\
                                                include_lowest=True, right=False)
-        data['start_year'] = pd.cut(data['start_year'],bins=yearGroups, labels=yearGroups[:-1],\
-                                               include_lowest=True, right=False)
-        data['end_year'] = pd.cut(data['end_year'],bins=yearGroups, labels=yearGroups[:-1],\
-                                               include_lowest=True, right=False)
+        if 'start_year' in data.columns:
+            data['start_year'] = pd.cut(data['start_year'],bins=yearGroups, labels=yearGroups[:-1],\
+                                        include_lowest=True, right=False)
+            
+        if 'end_year' in data.columns:
+            data['end_year'] = pd.cut(data['end_year'],bins=yearGroups, labels=yearGroups[:-1],\
+                                      include_lowest=True, right=False)
 
+        # within the group - all the multiple efforts need to be joined together
         data[criterion] = data.groupby(['year', 'author'])[criterion].transform(np.sum)
 
         print("data with duplicates: %s", data.shape[0])
         data = data.drop_duplicates()
         print("data without duplicates: %s", data.shape[0])
+        
+        print("data with null values: %s", data.shape[0])
+        data = data.dropna(how='any')
+        print("data without null values: %s", data.shape[0])
 
-    #print(data[data['author'] == 'donatella sciuto'].head(10))
+    #Sort the year and calculate the cumulative values
 
     data = data.set_index('year').sort_index()
     data['cum_'+criterion] = data.groupby(['author'])[criterion].transform(pd.Series.cumsum)
     data = data.reset_index()
-    print(data.head(10))
+    #print(data.head(10))
     return data
     
